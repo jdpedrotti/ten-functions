07fc8dd53d0959027fe8684ce593cd6be3829c2b
John Pedrotti <jdpedrotti@gmail.com>
2022-08-16 11:50:10 -0500

    completed-ten-functions-excersize

--- solutions.js
+++ solutions.js
@@ -24,3 +24,51 @@
 function isTrue(input){
     return input === true;
 }
+
+function isFalse(input){
+    return input === false;
+}
+
+function not(input){
+    return !input;
+}
+
+function addOne(input){
+    return Number(input) +1;
+}
+
+function isEven(input) {
+    if (input % 2 === 0) {
+        return true;
+    } else if (input % 2 !== 0){
+        return false;
+    }
+}
+
+function isIdentical(input1, input2){
+    if(input1 === input2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isEqual(value1, value2){
+    if(Number(value1 == value2)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function or(num1, num2) {
+    return (num1 || num2)
+}
+
+function and(and1, and2){
+    return(and1 && and2)
+}
+
+function concat(word1, word2) {
+    return (String(word1) + String(word2));
+}
